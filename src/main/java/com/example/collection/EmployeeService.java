049822bba0ac21f9a7e317e5611b9c44f325458e
Olesya1994 <olesia.liakhor@gmail.com>
2023-05-21 13:57:08 +0300

    first

--- src/main/java/com/example/collection/EmployeeService.java
+++ src/main/java/com/example/collection/EmployeeService.java
@@ -9,10 +9,10 @@ import java.util.List;
 @Service
 public class EmployeeService {
     private static final int MAX_SIZE = 10;
-    private List<Employee> employees = new ArrayList<>(MAX_SIZE);
+    private final List<Employee> employees = new ArrayList<>(MAX_SIZE);
 
 
-    public Employee add(String firstName, String lastName) throws EmployeeAlreadyAddedException, EmployeeStorageIsFullException {
+    public Employee add(String firstName, String lastName) {
         if (employees.size() >= MAX_SIZE) {
             throw new EmployeeStorageIsFullException();
         }
@@ -20,17 +20,21 @@ public class EmployeeService {
         if (employees.contains(employee)) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
+         employees.add(employee);
         return employee;
     }
 
     public Employee delete(String firstName, String lastName) throws EmployeeNotFoundException {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
-            employees.remove(employee);
-            return employee;
+        for (Employee empl : employees) {
+            if (empl.equals(employee)) {
+                employees.remove(employee);
+                return employee;
+            }
         }
+
         throw new EmployeeNotFoundException();
+
     }
 
 

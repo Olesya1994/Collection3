db7cfe3506bb192eba18e87b701a4f19e2aed315
Olesya1994 <olesia.liakhor@gmail.com>
2023-05-28 20:58:11 +0300

    + equals and hashCode

--- src/main/java/com/example/collection/Employee.java
+++ src/main/java/com/example/collection/Employee.java
@@ -1,5 +1,7 @@
 package com.example.collection;
 
+import java.util.Objects;
+
 public class Employee {
     private String firstName;
     private String lastName;
@@ -15,6 +17,19 @@ public class Employee {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 
     @Override
